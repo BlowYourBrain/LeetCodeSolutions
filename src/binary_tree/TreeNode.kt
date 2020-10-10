f41f3e1b3d73278620b47b130bbf9fad712e10bb
Evgeny Vasilev <fetel@yandex.ru>
2020-10-10 12:53:57 +0300

    added function to convert list to BinaryTree

--- src/binary_tree/TreeNode.kt
+++ src/binary_tree/TreeNode.kt
@@ -1,6 +1,95 @@
 package binary_tree
 
+import java.util.*
+
+fun main() {
+    val root = listToTreeNode(
+        listOf(5, 4, 8, 11, null, 13, 4, 7, 2, null, null, null, 1)
+    )
+
+}
+
 class TreeNode(var `val`: Int) {
-    val left: TreeNode? = null
-    val right: TreeNode? = null
+    var left: TreeNode? = null
+    var right: TreeNode? = null
+}
+
+fun listToTreeNode(list: List<Int?>): TreeNode {
+    val root = TreeNode(list.first()!!)
+    var lvl: Queue<TreeNode> = LinkedList<TreeNode>()
+
+    var currentNode = root
+
+    var isLeftNodeProcessed = false
+    var isRightNodeProcessed = false
+    var index = 1
+
+    while (true) {
+        if (isLeftNodeProcessed && isRightNodeProcessed) {
+            currentNode = lvl.poll()
+            isLeftNodeProcessed = false
+            isRightNodeProcessed = false
+        }
+
+        val item = list[index]
+
+        val newNode: TreeNode? =
+            if (item != null) {
+                val node = TreeNode(item)
+                lvl.add(node)
+                node
+            } else {
+                null
+            }
+
+        when {
+            !isLeftNodeProcessed -> {
+                currentNode.left = newNode
+                isLeftNodeProcessed = true
+            }
+
+            !isRightNodeProcessed -> {
+                currentNode.right = newNode
+                isRightNodeProcessed = true
+            }
+        }
+
+        if (list.lastIndex > index) {
+            index++
+        } else {
+            break
+        }
+    }
+
+    return root
+}
+
+fun <T> T.printlnResult(text: String? = null): T {
+    if (text == null) {
+        println("result is: ${this.toString()}")
+    } else {
+        println("$text ${this.toString()}")
+    }
+    return this
+}
+
+fun TreeNode.printTree() {
+    val maxDepth = MaxDepthSolution().maxDepth(this)
+    var maxNumber = 40
+    var maxNumberSymbolsCount = 1
+
+    while (maxNumber >= 10) {
+        maxNumber /= 10
+        maxNumberSymbolsCount++
+    }
+
+    val lineDividersCount = (maxNumberSymbolsCount + 1) / 2
+    val builder = StringBuilder()
+    printPosition(maxDepth, lineDividersCount)
+}
+
+private fun printPosition(depth: Int, lineDividersCount: Int): Int {
+    val dividers = lineDividersCount * (depth - 1)
+    val printPosition = depth * 2 + dividers
+    return printPosition
 }
---
+++ src/binary_tree/binary_tree_has_root_to_leaf_path.kt
@@ -0,0 +1,35 @@
+package binary_tree
+
+fun main() {
+    val root = listToTreeNode(
+        listOf(5, 4, 8, 11, null, 13, 4, 7, 2, null, null, null, 1)
+    )
+
+    HasRootToLeafPathSolution()
+        .hasPathSum(root, 22)
+        .printlnResult()
+        .let { assert(it) }
+}
+
+class HasRootToLeafPathSolution {
+
+    fun hasPathSum(root: TreeNode?, sum: Int): Boolean {
+        if (root == null) {
+            return false
+        }
+
+        return findLeaf(root, 0, sum)
+    }
+
+    private fun findLeaf(node: TreeNode, actualSum: Int, expextedSum: Int): Boolean {
+        if (node.left == null && node.right == null) {
+            return actualSum + node.`val` == expextedSum
+        }
+
+        node.left?.let { return findLeaf(it, actualSum + node.`val`, expextedSum) }
+        node.right?.let { return findLeaf(it, actualSum + node.`val`, expextedSum) }
+
+        return false
+    }
+
+}
