0c4d2fca3a2edc90cb27e33aa022b6e2a8717587
Васильев Евгений <fetel@yandex.ru>
2021-01-28 21:13:12 +0300

    Intersection Of Two Lists
    added another solution

--- src/linked_list/IntersectionOfTwoLists.kt
+++ src/linked_list/IntersectionOfTwoLists.kt
@@ -1,6 +1,7 @@
 package linked_list
 
 import company_interview_questions.add_two_numbers.ListNode
+import java.lang.Math.abs
 
 class IntersectionOfTwoLists {
 
@@ -33,4 +34,53 @@ class IntersectionOfTwoLists {
         return null
     }
 
+}
+
+class IntersectionOfTwoLists2 {
+    fun getIntersectionNode(headA:ListNode?, headB:ListNode?):ListNode? {
+        var curA = headA
+        var curB = headB
+        var curALength = 0
+        var curBLength = 0
+        while(curA != null || curB != null){
+            if (curA != null){
+                curALength++
+                curA = curA?.next
+            }
+
+            if (curB != null){
+                curBLength++
+                curB = curB?.next
+            }
+        }
+        val lengthDiff = curALength - curBLength
+
+        var (head1, head2) = if (lengthDiff !=0){
+            var (cur, otherCur) = if (lengthDiff >0){
+                headA to headB
+            }else{
+                headB to headA
+            }
+
+            val diff = kotlin.math.abs(lengthDiff)
+            var count = 0
+            while(count < diff){
+                cur = cur?.next
+                count++
+            }
+            cur to otherCur
+        } else {
+            headA to headB
+        }
+
+        while(head1 != null && head2 != null){
+            if (head1 == head2){
+                return head1
+            }
+            head1 = head1.next
+            head2 = head2.next
+        }
+
+        return null
+    }
 }
